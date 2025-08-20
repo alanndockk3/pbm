22dcdafded2b17b64b432e039601d858fce000c1
alanndockk3 <ajdoc3@hotmail.com>
2025-08-19 20:49:14 -0400

    updated favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Pretties by Marg",
   description: "Handcrafted each item",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
